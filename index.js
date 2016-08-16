86123827792f2148e234325fbf135cdcd187bd84
Diogo Basto <diogotbasto@gmail.com>
2016-08-16 07:21:38 -0400

    add start script and error testing

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(req,res) {
 		console.log(req.query.callback);
 		res.jsonp({"cards":[{"image":"http://i.imgur.com/eM2Uena.jpg","title":"Anamazingtitle","description":"Somethingaboutthiscard","ctas":[{"text":"125for1","value":"c1cta1"},{"text":"200for2","value":"c1cta2"}],"important":"yes"},{"title":"Anamazingtitle2","description":"Somethingaboutthisothercard","ctas":[{"text":"300","value":"c2"}]},{"title":"Didn'tlikeanything?","description":"Searchforsomethingelse","ctas":[{"text":"Search","value":"search"}],"important":"max"}]});
 	}
+	else if (req.query.with_errors !== undefined)
+	{
+		res.json({"cards":[{"ctas":[{"text":"125for1","value":"c1cta1"},{"text":"200for2","value":"c1cta2"}],"important":"yes"},{"ctas":[{"text":"300","value":"c2"}]},{"ctas":[{"text":"Search","value":"search"}],"important":"max"}]});
+	}
 	else
 	{
 		console.log("no callback");
